7db0fb6fbb2fee67ff39978797c3ba35284bd532
William14S <wsandoval7645@richmondhillhs.org>
2020-11-18 00:50:48 +0000

    yes

--- src/App.js
+++ src/App.js
@@ -5,20 +5,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>William Sandoval</h1>
+      <h2> All About Me</h2>
+      <p>I go by many names William , Liam, Willi, and Sandi</p>
+      <p>My favorite quote is from anime I watch that says "There is no such thing as perfect in this world. That may sound cliche, but it’s the truth. The average person admires perfection and seeks to obtain it. But what’s the point of achieving perfection? There is none. Nothing. Not a single thing. I spit on perfection! If something is perfect, then there is nothing left. There is no room for imagination. No place left for that person to gain additional knowledge or abilities. Do you know what that means? For scientists such as us, perfection only brings despair. It is our job to create things more wonderful than anything before them, but never to obtain perfection. A scientist must be a person who finds ecstasy while suffering from that antinomy."</p>
+      <p>My hobbies are music, working out, looking at business things such as stocks and real estate, and art</p>
     </div>
   );
 }
